51b89cc2830695816dc06f033146ee6fc39dbb51
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-05-23 20:56:46 -0300

    feat: draw a triangle

--- glwidget.cpp
+++ glwidget.cpp
@@ -6,11 +6,23 @@ GLWidget::GLWidget(QWidget *parent)
 
 void GLWidget::initializeGL()
 {
-
+    glClearColor(0.2f, 0.2f, 0.2f,1.0f);
 }
 
 void GLWidget::paintGL()
 {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(0.0f, 0.5f, 0.0f);
+
+        glColor3f(0.5f, 0.0f, 0.0f);
+        glVertex3f(0.5f, -0.5f, 0.0f);
+
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(-0.5f, -0.5f, 0.0f);
+    glEnd();
 
 }
 
